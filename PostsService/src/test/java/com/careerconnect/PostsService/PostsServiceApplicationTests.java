16714bec39eafb53811ad12b7ac5dd7f0a1c4fd8
Gagan Basnet <chatwidmeah@gmail.com>
2025-01-28 22:31:48 +0545

    added initial project setup for postsservice and userservice

---
+++ PostsService/src/test/java/com/careerconnect/PostsService/PostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerconnect.PostsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
