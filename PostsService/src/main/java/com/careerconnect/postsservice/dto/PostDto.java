a218cc3625596b4eea71b1040c0dc0aac8fcb803
Gagan Basnet <chatwidmeah@gmail.com>
2025-01-29 23:57:29 +0545

    added postdto, post entity and postlike entity

---
+++ PostsService/src/main/java/com/careerconnect/postsservice/dto/PostDto.java
@@ -0,0 +1,13 @@
+package com.careerconnect.postsservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String content;
+    private Long userId;
+    private LocalDateTime createdAt;
+}
