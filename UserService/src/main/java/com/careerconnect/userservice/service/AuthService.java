5678337082d1170ec2db1b027e2d6e2dd80dd3d1
Gagan Basnet <chatwidmeah@gmail.com>
2025-02-05 02:00:58 +0545

    completed signup implementation in AuthService, added jbcrypt dependency and PasswordUtil class for decrypting password

--- UserService/src/main/java/com/careerconnect/userservice/service/AuthService.java
+++ UserService/src/main/java/com/careerconnect/userservice/service/AuthService.java
@@ -2,9 +2,12 @@ package com.careerconnect.userservice.service;
 
 import com.careerconnect.userservice.dto.SignupRequestDto;
 import com.careerconnect.userservice.dto.UserDto;
+import com.careerconnect.userservice.entity.User;
 import com.careerconnect.userservice.repository.UserRepository;
+import com.careerconnect.userservice.utils.PasswordUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +15,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AuthService {
 
+  private final ModelMapper modelMapper;
   private UserRepository userRepository;
 
   public UserDto signUp(SignupRequestDto signupRequestDto) {
-    return null;
+    User user = modelMapper.map(signupRequestDto, User.class);
+    user.setPassword(PasswordUtil.hashPassword(signupRequestDto.getPassword()));
+
+    User savedUser = userRepository.save(user);
+    return modelMapper.map(savedUser, UserDto.class);
   }
 }
