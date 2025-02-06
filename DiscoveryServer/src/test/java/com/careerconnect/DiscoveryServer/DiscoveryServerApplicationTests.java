8c34acae6401928fb8251efb56ff3d285cd35908
Gagan Basnet <chatwidmeah@gmail.com>
2025-02-07 02:28:48 +0545

    added api gateway and discovery server

---
+++ DiscoveryServer/src/test/java/com/careerconnect/DiscoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.careerconnect.DiscoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
