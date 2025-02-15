577633ffe09ed843e2bf2603cee5c5e312e02b99
Gagan Basnet <chatwidmeah@gmail.com>
2025-02-15 21:23:11 +0545

    added modelmapper and spotless plugin in ConnectionsService

--- ConnectionsService/src/main/java/com/careerconnect/ConnectionsService/ConnectionsServiceApplication.java
+++ ConnectionsService/src/main/java/com/careerconnect/ConnectionsService/ConnectionsServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConnectionsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnectionsServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ConnectionsServiceApplication.class, args);
+  }
 }
--- ConnectionsService/src/test/java/com/careerconnect/ConnectionsService/ConnectionsServiceApplicationTests.java
+++ ConnectionsService/src/test/java/com/careerconnect/ConnectionsService/ConnectionsServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConnectionsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
