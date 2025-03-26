82f8f98e61cd14cae36d95bb79d4579937eb9e5e
Gagan Basnet <chatwidmeah@gmail.com>
2025-03-26 23:45:22 +0545

    implemented functionality to send notification when someone likes a post

--- PostsService/src/main/java/com/careerconnect/postsservice/service/PostLikeService.java
+++ PostsService/src/main/java/com/careerconnect/postsservice/service/PostLikeService.java
@@ -1,7 +1,9 @@
 package com.careerconnect.postsservice.service;
 
 import com.careerconnect.postsservice.auth.UserContextHolder;
+import com.careerconnect.postsservice.entity.Post;
 import com.careerconnect.postsservice.entity.PostLike;
+import com.careerconnect.postsservice.event.PostLikedEvent;
 import com.careerconnect.postsservice.exception.BadRequestException;
 import com.careerconnect.postsservice.exception.ResourceNotFoundException;
 import com.careerconnect.postsservice.repository.PostLikeRepository;
@@ -9,6 +11,7 @@ import com.careerconnect.postsservice.repository.PostsRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +21,16 @@ public class PostLikeService {
 
   private final PostLikeRepository postLikeRepository;
   private final PostsRepository postsRepository;
+  private final KafkaTemplate<Long, PostLikedEvent> kafkaTemplate;
 
   public void likePost(Long postId) {
     Long userId = getUserId();
     log.info("Attempting to like the post with id: " + postId);
 
-    boolean exists = postsRepository.existsById(postId);
-    if (!exists) throw new ResourceNotFoundException("Post not found with id:" + postId);
+    Post post =
+        postsRepository
+            .findById(postId)
+            .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
 
     boolean alreadyLiked = postLikeRepository.existsByUserIdAndPostId(userId, postId);
     if (alreadyLiked) throw new BadRequestException("Cannot Like the same post again.");
@@ -34,6 +40,15 @@ public class PostLikeService {
     postLike.setUserId(userId);
     postLikeRepository.save(postLike);
     log.info("Post with id: {} liked sucessfully", postId);
+
+    PostLikedEvent postLikedEvent =
+        PostLikedEvent.builder()
+            .postId(postId)
+            .likedByUserId(userId)
+            .creatorId(post.getUserId())
+            .build();
+
+    kafkaTemplate.send("post-liked-topic", postId, postLikedEvent);
   }
 
   @Transactional
