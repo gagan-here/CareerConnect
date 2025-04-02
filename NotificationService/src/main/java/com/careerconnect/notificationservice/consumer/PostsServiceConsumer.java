730660772e3549b5831458bf525ada8060784a17
Gagan Basnet <chatwidmeah@gmail.com>
2025-04-02 23:08:54 +0545

    added connections client to fetch first degree connections in PostsServiceConsumer

--- NotificationService/src/main/java/com/careerconnect/notificationservice/consumer/PostsServiceConsumer.java
+++ NotificationService/src/main/java/com/careerconnect/notificationservice/consumer/PostsServiceConsumer.java
@@ -1,14 +1,23 @@
 package com.careerconnect.notificationservice.consumer;
 
+import com.careerconnect.notificationservice.clients.ConnectionsClient;
+import com.careerconnect.notificationservice.dto.PersonDto;
 import com.careerconnect.postsservice.event.PostCreatedEvent;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PostsServiceConsumer {
 
+  private final ConnectionsClient connectionsClient;
+
   @KafkaListener(topics = "post-created-topic")
-  public void handlePostCreated(PostCreatedEvent postCreatedEvent) {}
+  public void handlePostCreated(PostCreatedEvent postCreatedEvent) {
+    List<PersonDto> firstConnections = connectionsClient.getFirstConnections();
+  }
 }
