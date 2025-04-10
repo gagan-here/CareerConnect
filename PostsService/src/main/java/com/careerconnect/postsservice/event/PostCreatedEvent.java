9ab59cd3a5cb516e5d5bced831ad2978f2571588
Gagan Basnet <chatwidmeah@gmail.com>
2025-04-11 00:02:02 +0545

    added constructor in PostCreatedEvent classes

--- PostsService/src/main/java/com/careerconnect/postsservice/event/PostCreatedEvent.java
+++ PostsService/src/main/java/com/careerconnect/postsservice/event/PostCreatedEvent.java
@@ -1,10 +1,14 @@
 package com.careerconnect.postsservice.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostCreatedEvent {
   Long creatorId;
   String content;
