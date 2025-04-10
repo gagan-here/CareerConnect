a6a71408924e59577a04608ebde3ab05f1d63fd0
Gagan Basnet <chatwidmeah@gmail.com>
2025-04-10 23:56:19 +0545

    added error handling deserializer in notification service

--- NotificationService/src/main/java/com/careerconnect/notificationservice/clients/ConnectionsClient.java
+++ NotificationService/src/main/java/com/careerconnect/notificationservice/clients/ConnectionsClient.java
@@ -4,10 +4,11 @@ import com.careerconnect.notificationservice.dto.PersonDto;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "ConnectionsService", path = "/connections")
 public interface ConnectionsClient {
 
   @GetMapping("/core/first-degree")
-  List<PersonDto> getFirstConnections(Long userId);
+  List<PersonDto> getFirstConnections(@RequestHeader("X-User-Id") Long userId);
 }
