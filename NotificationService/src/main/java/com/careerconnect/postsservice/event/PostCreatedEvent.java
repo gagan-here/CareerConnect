c6582a27d2e5a728243d5858c7aa19c81160a38e
Gagan Basnet <chatwidmeah@gmail.com>
2025-04-27 21:56:31 +0545

    removed unnecessary builder annotation

--- NotificationService/src/main/java/com/careerconnect/postsservice/event/PostCreatedEvent.java
+++ NotificationService/src/main/java/com/careerconnect/postsservice/event/PostCreatedEvent.java
@@ -1,12 +1,10 @@
 package com.careerconnect.postsservice.event;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostCreatedEvent {
--- NotificationService/src/main/java/com/careerconnect/postsservice/event/PostLikedEvent.java
+++ NotificationService/src/main/java/com/careerconnect/postsservice/event/PostLikedEvent.java
@@ -1,10 +1,8 @@
 package com.careerconnect.postsservice.event;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class PostLikedEvent {
   Long postId;
   Long creatorId;
